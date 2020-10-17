13e086be2b133477a23bf7951fcf5bf9d4cb6eb3
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2020-10-17 21:06:42 +0200

    Valid instance Object

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -21,10 +21,17 @@ class OracleOfBacon
 
   def from_does_not_equal_to
     # YOUR CODE HERE
+    if (from == to) 
+      errors.add(:from , "From cannot be the same as To")
+      errors.add(:to , "From cannot be the same as To")
+    end
   end
 
   def initialize(api_key='')
     # your code here
+    @api_key = api_key
+    @from = "Kevin Bacon"
+    @to   = "Kevin Bacon"
   end
 
   def find_connections
